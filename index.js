c30e207b6a6add52736494f5589caa0d2b77aeac
LucasOliverrrr <lucasouze@gmail.com>
2024-05-23 09:42:42 -0300

    commit 3

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const fs         = require("fs")
 const{text }     = require("express")
 
 const servidor = http.createServer((req, res) =>{
-    res.writeHead(200, {"Content-Type": "text/html"})
-    res.writeHead('<form action="envioDeArquivo" method = post enctype="multipart/form-data"')
+
+    if(req.url == '/envioDeArquivo'){
+
+    }else{
+        res.writeHead(200, {"Content-Type": "text/html"})
+        res.writeHead('<form action="envioDeArquivo" method = post enctype="multipart/form-data">')
+        res.write('<input type = "file" name = "filetoupload"><br>')
+        res.write('<input type="submit" value="Enviar">')
+        res.write('</form>')
+        return res.end()
+    }
+})
+ 
+servidor.listen(porta, () =>{
+    console.log("Servidor Rodando na porta 3000")
 })
